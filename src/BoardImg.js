cdd2658cec6ea538cb446c8046348a2cdf2ab291
omniscient <omnisci3nce@protonmail.com>
2021-01-10 22:32:44 +1100

    show grid on hover

--- src/BoardImg.js
+++ src/BoardImg.js
@@ -1,9 +1,9 @@
 const MAX_H = 256
 const MAX_W = 256
 
-const BoardImg = ({image, onHover}) => {
+const BoardImg = ({image, onHover, onEndHover}) => {
   return (
-    <span onClick={onHover}>
+    <span onMouseEnter={onHover} onMouseLeave={onEndHover}>
       <img style={{ maxWidth: MAX_W, maxHeight: MAX_H, objectFit: 'cover' }} src={image} />
     </span>
   )
